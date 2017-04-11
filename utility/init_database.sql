f1f66d18ec1970ff0f4bc6c640565f9e014808bc
LyqSerenity <root@lyq.me>
2017-04-12 00:19:27 +0800

    Change init_database.sql.

--- utility/init_database.sql
+++ utility/init_database.sql
@@ -1,37 +1,37 @@
 use bless;
-drop table if exists users;
-drop table if exists events;
-drop table if exists event_user;
-drop table if exists login_token;
-drop table if exists event_blessings;
+drop table if exists `users`;
+drop table if exists `events`;
+drop table if exists `event_user`;
+drop table if exists `login_token`;
+drop table if exists `event_blessings`;
 
-create table users (
+create table `users` (
     EMAIL varchar(50) not null,
     FULLNAME varchar(20) not null,
     BIRTHDAY date,
     primary key (EMAIL)
 );
 
-create table events (
+create table `events` (
     ID int not null auto_increment,
     DESCRIPTION varchar(100) not null,
     HAPPEN_DATE date not null,
     primary key (ID)
 );
 
-create table event_user (
+create table `event_user` (
     EVENT_ID int not null,
     EMAIL varchar(50) not null,
     EDIT_TIME timestamp not null default current_timestamp,
     primary key (EVENT_ID, EMAIL)
 );
 
-create table login_token (
+create table `login_token` (
     TOKEN int not null,
     primary key (TOKEN)
 );
 
-create table event_blessings (
+create table `event_blessings` (
     EVENT_ID int not null,
     EMAIL varchar(50) not null,
     MESSAGE varchar(300) not null,
