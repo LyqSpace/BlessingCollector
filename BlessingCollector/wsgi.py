478dbc7aa37aa9050d4227e8abad9b7d48f4058a
LyqSpace <root@lyq.me>
2018-10-13 02:26:10 -0400

    remove __pychache__

--- BlessingCollector/wsgi.py
+++ BlessingCollector/wsgi.py
@@ -8,11 +8,17 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+from os.path import dirname, abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.append(PROJECT_DIR)
 
 from django.core.wsgi import get_wsgi_application
 from utility.functions import init
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BlessingCollector.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BlessingCollector.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "BlessingCollector.settings"
 
 init()
 
